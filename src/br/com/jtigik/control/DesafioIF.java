0523a63d0919902c390f6b00f3396eb793663840
jtigik <jeftigik@hotmail.com>
2024-08-18 20:52:35 -0300

    New Source Packages added

---
+++ src/br/com/jtigik/control/DesafioIF.java
@@ -0,0 +1,15 @@
+package br.com.jtigik.control;
+
+public class DesafioIF {
+
+    public static void main(String[] args) {
+        double nota = 2.7;
+
+        if (nota >= 9.0)
+            ;
+        {
+            System.out.println("Quadro de honra!");
+            System.out.println("Você é um exemplo!!!");
+        }
+    }
+}
