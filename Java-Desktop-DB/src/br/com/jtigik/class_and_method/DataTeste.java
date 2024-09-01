a8c9af2940b956697eda8c7562ce9d33a994d5c2
jtigik <jeftigik@hotmail.com>
2024-09-01 02:52:44 -0300

    Improving Class 'Data'

--- Java-Desktop-DB/src/br/com/jtigik/class_and_method/DataTeste.java
+++ Java-Desktop-DB/src/br/com/jtigik/class_and_method/DataTeste.java
@@ -7,17 +7,9 @@ public class DataTeste {
         d1.mes = 11;
         d1.ano = 2023;
 
-        Data d2 = new Data();
-
-        d2.dia = 3;
-        d2.mes = 10;
-        d2.ano = 2019;
+        Data d2 = new Data(3, 10, 2019);
 
         Data d3 = new Data();
-
-        d3.dia = 14;
-        d3.mes = 9;
-        d3.ano = 2010;
 
         System.out.printf("%d.%d.%d\n", d1.dia, d1.mes, d1.ano);
 
