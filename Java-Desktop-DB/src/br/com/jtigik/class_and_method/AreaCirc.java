70533224d83e34233e3764bfdb14d96214086359
jtigik <jeftigik@hotmail.com>
2024-09-01 21:51:46 -0300

    Added new features to static method & varible Class

--- Java-Desktop-DB/src/br/com/jtigik/class_and_method/AreaCirc.java
+++ Java-Desktop-DB/src/br/com/jtigik/class_and_method/AreaCirc.java
@@ -2,14 +2,17 @@ package br.com.jtigik.class_and_method;
 
 public class AreaCirc {
     double raio;
-    static double pi;
+    final static double PI = 3.1415;
 
     AreaCirc(double raioInicial) {
-        pi = 3.14;
         raio = raioInicial;
     }
 
     double area() {
-        return pi * Math.pow(raio, 2);
+        return PI * Math.pow(raio, 2);
+    }
+
+    static double area(double raio) {
+        return PI * Math.pow(raio, 2);
     }
 }
