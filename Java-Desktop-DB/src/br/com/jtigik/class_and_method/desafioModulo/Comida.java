13245214187fa81d1b9d7dc9e7383dd26e4d8095
jtigik <jeftigik@hotmail.com>
2024-09-03 17:41:03 -0300

    Added Coding challenge

---
+++ Java-Desktop-DB/src/br/com/jtigik/class_and_method/desafioModulo/Comida.java
@@ -0,0 +1,11 @@
+package br.com.jtigik.class_and_method.desafioModulo;
+
+public class Comida {
+    String nome;
+    double peso;
+
+    Comida(String nome, double peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+}
---
+++ Java-Desktop-DB/src/br/com/jtigik/class_and_method/desafioModulo/Refeicao.java
@@ -0,0 +1,46 @@
+package br.com.jtigik.class_and_method.desafioModulo;
+
+import javax.swing.JOptionPane;
+
+public class Refeicao {
+    public static void main(String[] args) {
+        Comida c1 = new Comida("Arroz", 0.180);
+        Comida c2 = new Comida("Feijão", 0.300);
+
+        Pessoa p1 = new Pessoa("João", 99.8);
+
+        JOptionPane.showMessageDialog(
+                null,
+                p1.apresentar(),
+                "Jantar",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        p1.comer(c1);
+
+        JOptionPane.showMessageDialog(
+                null,
+                p1.apresentar(),
+                "Jantar",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        p1.comer(c2);
+
+        JOptionPane.showMessageDialog(
+                null,
+                p1.apresentar(),
+                "Jantar",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        p1.comer(c1);
+
+        /*
+         * if (p1.peso <= 100)
+         * JOptionPane.showMessageDialog(null, p1.apresentar(), "Jantar",
+         * JOptionPane.INFORMATION_MESSAGE);
+         * else
+         * JOptionPane.showMessageDialog(null, p1.apresentar(), "Jantar",
+         * JOptionPane.ERROR_MESSAGE);
+         */
+
+    }
+}
