de72412f76b5161f1f37e031d5b364eed709c997
jtigik <jeftigik@hotmail.com>
2024-09-05 01:35:20 -0300

    Added matrix structure

---
+++ Java-Desktop-DB/src/br/com/jtigik/class_and_method/MatrizNota.java
@@ -0,0 +1,45 @@
+package br.com.jtigik.class_and_method;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class MatrizNota {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Quantos alunos?: ");
+        int qtdAlunos = entrada.nextInt();
+
+        System.out.println("Quantas notas por aluno?: ");
+        int qtdNotas = entrada.nextInt();
+
+        double[][] notasDaTurma = new double[qtdAlunos][qtdNotas];
+
+        int total = 0;
+
+        for (int i = 0; i < notasDaTurma.length; i++) {// Representa os alunos
+            for (int j = 0; j < notasDaTurma[i].length; j++) {//Representa as notas de cada aluno.
+                System.out.printf("Informe a nota %d do Aluno %s: ", j + 1, i + 12);
+
+                notasDaTurma[i][j] = entrada.nextDouble();
+                total += notasDaTurma[i][j];
+            }
+        }
+        double media = total / (qtdAlunos * qtdNotas);
+
+        System.out.println();
+
+        for (double[] notasAluno : notasDaTurma) {
+            System.out.println(Arrays.toString(notasAluno));
+        }
+
+        JOptionPane.showMessageDialog(null,
+                String.format("A média da Turma é: %.1f", media),
+                "Resultado Final",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        entrada.close();
+    }
+}
