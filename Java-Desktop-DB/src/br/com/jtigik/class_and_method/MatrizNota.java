209581556ff7bec0c4cbae2215fc0d1ac479cb20
jtigik <jeftigik@hotmail.com>
2024-09-05 18:05:25 -0300

    Added try-except block

--- Java-Desktop-DB/src/br/com/jtigik/class_and_method/MatrizNota.java
+++ Java-Desktop-DB/src/br/com/jtigik/class_and_method/MatrizNota.java
@@ -7,39 +7,37 @@ import javax.swing.JOptionPane;
 public class MatrizNota {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Quantos alunos?: ");
+            int qtdAlunos = entrada.nextInt();
 
-        System.out.println("Quantos alunos?: ");
-        int qtdAlunos = entrada.nextInt();
+            System.out.println("Quantas notas por aluno?: ");
+            int qtdNotas = entrada.nextInt();
 
-        System.out.println("Quantas notas por aluno?: ");
-        int qtdNotas = entrada.nextInt();
+            double[][] notasDaTurma = new double[qtdAlunos][qtdNotas];
 
-        double[][] notasDaTurma = new double[qtdAlunos][qtdNotas];
+            int total = 0;
 
-        int total = 0;
+            for (int i = 0; i < notasDaTurma.length; i++) {// Representa os alunos
+                for (int j = 0; j < notasDaTurma[i].length; j++) {//Representa as notas de cada aluno.
+                    System.out.printf("Informe a nota %d do Aluno %s: ", j + 1, i + 12);
 
-        for (int i = 0; i < notasDaTurma.length; i++) {// Representa os alunos
-            for (int j = 0; j < notasDaTurma[i].length; j++) {//Representa as notas de cada aluno.
-                System.out.printf("Informe a nota %d do Aluno %s: ", j + 1, i + 12);
-
-                notasDaTurma[i][j] = entrada.nextDouble();
-                total += notasDaTurma[i][j];
+                    notasDaTurma[i][j] = entrada.nextDouble();
+                    total += notasDaTurma[i][j];
+                }
             }
+            double media = total / (qtdAlunos * qtdNotas);
+
+            System.out.println();
+
+            for (double[] notasAluno : notasDaTurma) {
+                System.out.println(Arrays.toString(notasAluno));
+            }
+
+            JOptionPane.showMessageDialog(null,
+                    String.format("A média da Turma é: %.1f", media),
+                    "Resultado Final",
+                    JOptionPane.INFORMATION_MESSAGE);
         }
-        double media = total / (qtdAlunos * qtdNotas);
-
-        System.out.println();
-
-        for (double[] notasAluno : notasDaTurma) {
-            System.out.println(Arrays.toString(notasAluno));
-        }
-
-        JOptionPane.showMessageDialog(null,
-                String.format("A média da Turma é: %.1f", media),
-                "Resultado Final",
-                JOptionPane.INFORMATION_MESSAGE);
-
-        entrada.close();
     }
 }
