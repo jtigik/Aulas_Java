5f89a9a94a19d5ab67148914bfc49afc9909ff7a
jtigik <jeftigik@hotmail.com>
2024-09-08 02:31:35 -0300

    Added Matrix Challenger

---
+++ Java-Desktop-DB/src/br/com/jtigik/class_and_method/MatrizDiagonalExe.java
@@ -0,0 +1,18 @@
+package br.com.jtigik.class_and_method;
+
+public class MatrizDiagonalExe {
+
+    public static void MatrixDiagonal() {
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("%d ", i == j ? 1 : 0);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        MatrixDiagonal();
+    }
+}
