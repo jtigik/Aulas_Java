eea8b20a4380bd62c34fe6c4b9540fb82a0dd49c
jtigik <jeftigik@hotmail.com>
2024-09-24 17:08:02 -0300

    Improving Buttom & Key Classes

--- Java-Desktop-DB/src/br/com/jtigik/swing/calc/visao/Botao.java
+++ Java-Desktop-DB/src/br/com/jtigik/swing/calc/visao/Botao.java
@@ -1,10 +1,23 @@
 package br.com.jtigik.swing.calc.visao;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 public class Botao extends JButton {
 
-    public Botao() {
+    public Botao(String texto, Color cor) {
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
+
+        setForeground(Color.WHITE);
+
+        setFont(new Font("consolas", Font.PLAIN, 25));
+
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
     }
 
 }
