da87c91e671d8210e49eea1be94633c57f172c59
jtigik <jeftigik@hotmail.com>
2024-09-29 22:13:24 -0300

    Added JDBC Class

---
+++ Java-Desktop-DB/src/br/com/jtigik/swing/JDBC/TesteConexao.java
@@ -0,0 +1,18 @@
+package br.com.jtigik.swing.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+
+    public static void main(String[] args) throws SQLException {
+        final String url = "jdbc:mysql://localhost";
+        final String usuario = "root";
+        final String senha = "";
+
+        try (Connection conexao = DriverManager.getConnection(url, usuario, senha)) {
+            System.out.println("Conexão efetuada com sucesso!");
+        }
+    }
+}
