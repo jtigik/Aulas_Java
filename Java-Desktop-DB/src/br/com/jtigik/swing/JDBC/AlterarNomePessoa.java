cb06b88857dcccb3fa3be78fdb801435f013ee0e
jtigik <jeftigik@hotmail.com>
2024-10-04 23:34:27 -0300

    Class Update 2nd

--- Java-Desktop-DB/src/br/com/jtigik/swing/JDBC/AlterarNomePessoa.java
+++ Java-Desktop-DB/src/br/com/jtigik/swing/JDBC/AlterarNomePessoa.java
@@ -8,33 +8,27 @@ import java.util.Scanner;
 
 public class AlterarNomePessoa {
 
-    @SuppressWarnings("static-access")
     public static void main(String[] args) throws SQLException {
-        Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Informe o código da Pessoa: ");
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Informe o código da pessoa: ");
         int codigo = entrada.nextInt();
 
-        String sql = "SELECT * FROM pessoa WHERE codigo = ?";
+        String sql = "SELECT * FROM pessoa WHERE CÓDIGO = ?";
 
-        Connection conexao = new FabricaDeConexao().getConexao();
-
+        Connection conexao = FabricaDeConexao.getConexao();
         PreparedStatement stmt = conexao.prepareStatement(sql);
         stmt.setInt(1, codigo);
-        ResultSet res = stmt.executeQuery();
+        ResultSet resultado = stmt.executeQuery();
 
-        if (res.next()) {
-            Pessoa p = new Pessoa(res.getInt(1), res.getString(2));
-            System.out.println("O nome atual é: " + p.getNome());
-
-            System.out.println("Informe outro número: ");
-            String novoNome = entrada.nextLine();
-
-            sql = "UPDATE pessoa SET nome = ? WHERE";
-            entrada.close();
-            stmt.close();
-            stmt = conexao.prepareStatement(sql);
+        if (resultado.next()) {
+            Pessoa p = new Pessoa(
+                    resultado.getInt(1),
+                    resultado.getString(2));
 
         }
+
+        conexao.close();
+        entrada.close();
     }
 }
