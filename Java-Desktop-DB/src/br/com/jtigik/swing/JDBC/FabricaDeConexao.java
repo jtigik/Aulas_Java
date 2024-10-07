c0e3ad33799d659a547da0133f5f061240066d38
jtigik <jeftigik@hotmail.com>
2024-10-07 01:21:24 -0300

    Configuring External Config. File.

--- Java-Desktop-DB/src/br/com/jtigik/swing/JDBC/FabricaDeConexao.java
+++ Java-Desktop-DB/src/br/com/jtigik/swing/JDBC/FabricaDeConexao.java
@@ -1,21 +1,31 @@
 package br.com.jtigik.swing.JDBC;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaDeConexao {
 
     public static Connection getConexao() {
         try {
-            final String url
-                    = "jdbc:mysql://localhost/javaDesktop?verifyServerCertificate=false&useSSL=true";
-            final String usuario = "root";
-            final String senha = "874592";
+            Properties prop = getProperties();
+            final String url = prop.getProperty("banco.url");
+            final String usuario = prop.getProperty("banco.usuario");
+            final String senha = prop.getProperty("banco.senha");
 
             return DriverManager.getConnection(url, usuario, senha);
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Properties getProperties() throws IOException {
+        Properties prop = new Properties();
+        String caminho = "/conexao.properties";
+
+        prop.load(FabricaDeConexao.class.getResourceAsStream(caminho));
+        return prop;
     }
 }
