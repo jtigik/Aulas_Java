bcd205a60aa3a89cf935171c819bfecb8fbb38ca
jtigik <jeftigik@hotmail.com>
2024-10-12 22:43:23 -0300

    Updating Create DB & Add new register

--- Java-Desktop-DB/src/br/com/jtigik/swing/JDBC/Criar_BD.java
+++ Java-Desktop-DB/src/br/com/jtigik/swing/JDBC/Criar_BD.java
@@ -7,18 +7,16 @@ import java.sql.Statement;
 public class Criar_BD {
 
     public static void main(String[] args) throws SQLException {
-        /* final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
-        final String usuario = "root";
-        final String senha = "874592";
-
-        Connection conexao = DriverManager.getConnection(url, usuario, senha); */
 
         Connection conexao = FabricaDeConexao.getConexao();
 
         Statement stmt = conexao.createStatement();
-        stmt.execute("CREATE DATABASE IF NOT EXISTS javaDesktop");
 
-        System.out.println("Banco criado com sucesso!");
+        stmt.execute(
+                "CREATE DATABASE IF NOT EXISTS javaDesktop");
+
+        System.out.println(
+                "Banco criado com sucesso!");
 
         conexao.close();
     }
